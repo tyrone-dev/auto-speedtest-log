2d5c6eff6dcf73b13f1c157018a08a66d7f7b284
tyronevb <tyronevb@gmail.com>
2016-08-11 17:11:46 +0200

    Write speed values to Google Sheet
    Added functionality to write the speed test results to a Google Sheet.
    The required client_id.json files are not included here and should be
    generated using the Google Developer Console.
    
    Currently in progress. Issues: a new sheet is created each time the
    script is run as opposed to adding a new row to the worksheet.

--- auto_speedtest_logger.py
+++ auto_speedtest_logger.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python
 
+import argparse
 import subprocess
 import time
+
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+isp = 'test'
+service = 'fibre'
+service_number = 'null'
+
+sheet_title = '{} {} - {}'.format(isp, service, service_number)
+
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+store = file.Storage('storage.json')
+credentials = store.get()
+if not credentials or credentials.invalid:
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+    credentials = tools.run_flow(flow, store, flags)
+
+SHEETS = discovery.build('sheets', 'v4', http=credentials.authorize(Http()))
+data = {'properties': {'title': sheet_title}}
+res = SHEETS.spreadsheets().create(body=data).execute()
+SHEET_ID = res['spreadsheetId']
+
+print 'Created {}'.format(res['properties']['title'])
+
+FIELDS = ('Timestamp', 'Ping (ms)', 'Download (Mbps)', 'Upload (Mbps)')
 
 process = subprocess.Popen('/usr/local/bin/speedtest-cli --simple'.split(),
         stdout=subprocess.PIPE)
@@ -15,3 +43,19 @@ values.insert(0, start_time)
 
 with open('speedtest.log', 'a') as logfile:
     logfile.write(','.join(values) + '\n')
+
+values = [tuple(values)]
+values.insert(0, FIELDS)
+data = {'values': [row for row in values]}
+
+SHEETS.spreadsheets().values().update(spreadsheetId=SHEET_ID, range='A1',
+        body=data, valueInputOption='RAW').execute()
+
+print 'Wrote data to Sheet:'
+
+rows = SHEETS.spreadsheets().values().get(spreadsheetId=SHEET_ID,
+range='Sheet1').execute().get('values', [])
+for row in rows:
+    print row
+
+
