b7cfa9e027f9595d03848f20c4cbf05d5b13016f
tyronevb <tyronevb@gmail.com>
2016-08-13 15:25:00 +0200

    Append new results to existing sheet
    Checks if a spreadsheet was previously created for the speedtest
    results. If not, creates a new sheet in the linked Google Drive and
    stores the spreadsheet id in a file locally.
    
    If a spreadsheet was created previously, use that as the spreadsheet to
    work on.
    
    New results are now appended to an existing spreadsheet.
    
    Code was also refactored quite a bit to improve functionality.

--- auto_speedtest_logger.py
+++ auto_speedtest_logger.py
@@ -3,59 +3,121 @@
 import argparse
 import subprocess
 import time
+import os
 
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-isp = 'test'
-service = 'fibre'
-service_number = 'null'
+isp = 'Afrihost'
+service = '20 Mbps ADSL2+'
+service_number = '021 712 4225'
 
-sheet_title = '{} {} - {}'.format(isp, service, service_number)
+class SheetHandler(object):
+    def __init__(self):
+        self.SHEETS = self.connect_to_sheets()
 
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-store = file.Storage('storage.json')
-credentials = store.get()
-if not credentials or credentials.invalid:
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
-    credentials = tools.run_flow(flow, store, flags)
+        # check if a spreadsheet was already created
+        if os.path.isfile('spreadsheetId.txt'):
+            self.SPREADSHEET_ID = open('spreadsheetId.txt', 'r').read().rstrip()
+        else:
+            spreadsheet_title = '{} {} - {}'.format(isp, service, service_number)
+            self.SPREADSHEET_ID = self.create_new_spreadsheet(spreadsheet_title)
 
-SHEETS = discovery.build('sheets', 'v4', http=credentials.authorize(Http()))
-data = {'properties': {'title': sheet_title}}
-res = SHEETS.spreadsheets().create(body=data).execute()
-SHEET_ID = res['spreadsheetId']
+            # store the spreadsheet id for future use
+            with open('spreadsheetId.txt', 'w') as sheetid_file:
+                sheetid_file.write(self.SPREADSHEET_ID)
 
-print 'Created {}'.format(res['properties']['title'])
+            # create spreadsheet headings
+            FIELDS = ['Timestamp', 'Ping (ms)', 'Download (Mbps)', 'Upload (Mbps)']
+            self.write_to_sheet(FIELDS)
 
-FIELDS = ('Timestamp', 'Ping (ms)', 'Download (Mbps)', 'Upload (Mbps)')
+    def create_new_spreadsheet(self, spreadsheet_title):
+        '''
+        Creates a new spreadsheet on the google drive
+        :param: spreadsheet_title: title for the spreadsheet
+        :return: spreadsheet_id
+        '''
 
-process = subprocess.Popen('/usr/local/bin/speedtest-cli --simple'.split(),
-        stdout=subprocess.PIPE)
-
-start_time = time.strftime('%d-%m-%Y %H:%M:%S')
-
-output = process.communicate()[0]
-
-values = [value.split(' ')[1] for value in output.strip().split('\n')]
-values.insert(0, start_time)
-
-with open('speedtest.log', 'a') as logfile:
-    logfile.write(','.join(values) + '\n')
-
-values = [tuple(values)]
-values.insert(0, FIELDS)
-data = {'values': [row for row in values]}
-
-SHEETS.spreadsheets().values().update(spreadsheetId=SHEET_ID, range='A1',
-        body=data, valueInputOption='RAW').execute()
-
-print 'Wrote data to Sheet:'
-
-rows = SHEETS.spreadsheets().values().get(spreadsheetId=SHEET_ID,
-range='Sheet1').execute().get('values', [])
-for row in rows:
-    print row
+        data = {'properties': {'title': spreadsheet_title}}
+        res = self.SHEETS.spreadsheets().create(body=data).execute()
+        SPREADSHEET_ID = res['spreadsheetId']
 
 
+        return SPREADSHEET_ID
+
+    def connect_to_sheets(self):
+        '''
+        Establish a connection to Google Sheets
+        and gain authorization
+        :return: sheets service endpoint
+        '''
+
+        SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+        store = file.Storage('storage.json')
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+            flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+            credentials = tools.run_flow(flow, store, flags)
+
+        # create Sheets service endpoint
+        SHEETS = discovery.build('sheets', 'v4', http=credentials.authorize(Http()))
+
+        return SHEETS
+
+    def write_to_sheet(self, data):
+        '''
+        Write data to a spreadsheet
+        :param: data: data to write to sheet
+        :return: nothing
+        '''
+        # data must be an array of arrays
+        data = [data]
+
+        data = {'values': [row for row in data]}
+
+        self.SHEETS.spreadsheets().values().append(spreadsheetId=self.SPREADSHEET_ID, range='A1',
+                body=data, valueInputOption='RAW').execute()
+
+    def read_from_sheet(self):
+        '''
+        Read data from a given spreadsheet
+        :return: data read, as array of rows
+        '''
+
+        rows = self.SHEETS.spreadsheets().values().get(spreadsheetId=self.SPREADSHEET_ID,
+            range='Sheet1').execute().get('values', [])
+
+        return rows
+
+    def run_speedtest(self):
+        '''
+        Run the speedtest and write the results to the Google Sheet
+        :return: nothing
+        '''
+
+        process = subprocess.Popen('/usr/local/bin/speedtest-cli --simple'.split(),
+                stdout=subprocess.PIPE)
+
+        # timestamp
+        start_time = time.strftime('%d-%m-%Y %H:%M:%S')
+
+        output = process.communicate()[0]
+
+        values = [value.split(' ')[1] for value in output.strip().split('\n')]
+        values.insert(0, start_time)
+
+        # write speedtest results to the spreadsheet
+        self.write_to_sheet(values)
+
+        # write to local file as debug
+        with open('speedtest.log', 'a') as logfile:
+            logfile.write(','.join(values) + '\n')
+
+# main
+# create object
+speed_test_sheet = SheetHandler()
+
+# run speed test
+speed_test_sheet.run_speedtest()
